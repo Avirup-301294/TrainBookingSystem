3ddb55f800d3ca8211b2919b9482f9f6150634ec
unknown <avirupmondal1994@gmail.com>
2024-03-02 22:02:20 +0530

    done features

--- src/main/java/com/avirup/booking/Model/TicketUpdateRequest.java
+++ src/main/java/com/avirup/booking/Model/TicketUpdateRequest.java
@@ -1,23 +1,9 @@
 package com.avirup.booking.Model;
 
+import lombok.Data;
+
+@Data
 public class TicketUpdateRequest {
     private Long ticketId;
     private String modifiedSeatNumber;
-
-    // Getters and setters for the fields
-    public Long getTicketId() {
-        return ticketId;
-    }
-
-    public void setTicketId(Long ticketId) {
-        this.ticketId = ticketId;
-    }
-
-    public String getModifiedSeatNumber() {
-        return modifiedSeatNumber;
-    }
-
-    public void setModifiedSeatNumber(String modifiedSeatNumber) {
-        this.modifiedSeatNumber = modifiedSeatNumber;
-    }
 }
